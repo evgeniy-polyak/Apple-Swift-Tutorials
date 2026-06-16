b6e991cea91bb021f2eaf1afe852af78c2428c2b
Evgeniy <evgeniy.polyak@gmail.com>
2026-06-16 11:30:01 +0300

    Первоая часть законченна

--- Block-05/ScoreKeeper/ScoreKeeper/ContentView.swift
+++ Block-05/ScoreKeeper/ScoreKeeper/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var playerList: [Player] = [
+        Player(name: "Евгений", score: 10),
+        Player(name: "Жанна", score: 15),
+    ]
+//    @State private var newPlayer: Player? = nil
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Score Keeper")
+                .font(.title.lowercaseSmallCaps())
+                .padding()
+            
+            Grid {
+                GridRow {
+                    Text("Player")
+                        .gridColumnAlignment(.leading)
+                    Text("Score")
+                }
+                .font(.headline)
+                
+                ForEach($playerList) { $player in
+                    GridRow {
+                        TextField("Name", text: $player.name)
+                        Text("\(player.score)")
+                        Stepper("", value: $player.score)
+                            .labelsHidden()
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+            .background(.secondary.opacity(0.1))
+            
+            
+            
+            Button {
+                withAnimation {
+                    playerList.append(Player(name: "", score: 0))
+                }
+                
+            } label: {
+                HStack {
+                    Image(systemName: "plus")
+                    Text("Add player")
+                }.padding(5)
+            }
+            .buttonStyle(.borderedProminent)
+            .padding(.top)
         }
-        .padding()
+        .padding(5)
     }
 }
 
