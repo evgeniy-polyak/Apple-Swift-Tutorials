cb6e4c7d0d8975d6d6f27af7a7c031e63551ed94
Evgeniy <evgeniy.polyak@gmail.com>
2026-06-16 13:54:12 +0300

    Testing выполнено!

--- Block-05/ScoreKeeper/ScoreKeeper/ContentView.swift
+++ Block-05/ScoreKeeper/ScoreKeeper/ContentView.swift
@@ -8,54 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var playerList: [Player] = [
-        Player(name: "Евгений", score: 10),
-        Player(name: "Жанна", score: 15),
-    ]
-//    @State private var newPlayer: Player? = nil
+    @State private var scoreboard: Scoreboard = Scoreboard()
+    @State private var startingPoints: Int = 0
+    @State private var highestScoreWin = true
 
     var body: some View {
         VStack {
             Text("Score Keeper")
                 .font(.title.lowercaseSmallCaps())
                 .padding()
-            
-            Grid {
-                GridRow {
-                    Text("Player")
-                        .gridColumnAlignment(.leading)
-                    Text("Score")
-                }
-                .font(.headline)
-                
-                ForEach($playerList) { $player in
+
+            GameSettingsView(
+                startingPoints: $startingPoints,
+                highestScoeWind: $highestScoreWin
+            )
+            .disabled(scoreboard.gameState != .setup)
+
+            VStack(alignment: .leading) {
+                Grid {
                     GridRow {
-                        TextField("Name", text: $player.name)
-                        Text("\(player.score)")
-                        Stepper("", value: $player.score)
-                            .labelsHidden()
+                        Text("Player")
+                            .gridColumnAlignment(.leading)
+                        Text("Score")
+                            .opacity(scoreboard.gameState == .setup ? 0 : 1.0)
+                    }
+                    .font(.headline)
+
+                    ForEach($scoreboard.players) { $player in
+                        GridRow {
+                            TextField("Name", text: $player.name)
+                            Text("\(player.score)")
+                                .opacity(
+                                    scoreboard.gameState == .setup ? 0 : 1.0)
+                            Stepper("", value: $player.score)
+                                .labelsHidden()
+                                .opacity(
+                                    scoreboard.gameState == .setup ? 0 : 1.0)
+                        }
                     }
                 }
-                Spacer()
-            }
-            .padding()
-            .background(.secondary.opacity(0.1))
-            
-            
-            
-            Button {
-                withAnimation {
-                    playerList.append(Player(name: "", score: 0))
+                .padding()
+
+                Button {
+                    withAnimation {
+                        scoreboard.players.append(Player(name: "", score: 0))
+                    }
+
+                } label: {
+                    HStack {
+                        Image(systemName: "plus")
+                        Text("Add player")
+                    }.padding(5)
                 }
-                
-            } label: {
-                HStack {
-                    Image(systemName: "plus")
-                    Text("Add player")
-                }.padding(5)
+                //                .buttonStyle(.borderedProminent)
+                .padding(.top)
+                .padding(.horizontal)
+                .opacity(scoreboard.gameState == .setup ? 1.0 : 0)
             }
-            .buttonStyle(.borderedProminent)
-            .padding(.top)
+
+            Spacer()
+
+            HStack {
+                switch scoreboard.gameState {
+                case .setup:
+                    Button("Start Game", systemImage: "play.fill") {
+                        scoreboard.gameState = .playing
+                        scoreboard.resetScores(to: startingPoints)
+
+                    }
+                case .playing:
+                    Button("End Game", systemImage: "stop.fill") {
+                        scoreboard.gameState = .gameOver
+                    }
+                case .gameOver:
+                    Button("Reset Game", systemImage: "arrow.counterclockwise")
+                    {
+                        scoreboard.gameState = .setup
+                    }
+                }
+            }
+            .buttonStyle(.bordered)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            .tint(.blue)
+
         }
         .padding(5)
     }
--- Block-05/ScoreKeeper/ScoreKeeper/Models.swift
+++ Block-05/ScoreKeeper/ScoreKeeper/Models.swift
@@ -18,3 +18,61 @@ struct Player: Identifiable {
         self.score = score
     }
 }
+
+extension Player: Equatable {
+    static func == (left: Player, right: Player) -> Bool {
+        left.name == right.name && left.score == right.score
+    }
+}
+
+enum GameSate {
+    case setup
+    case playing
+    case gameOver
+}
+
+
+struct Scoreboard {
+    var players: [Player] = [
+        Player(name: "Евгений", score: 10),
+        Player(name: "Жанна", score: 15),
+    ]
+    
+    var gameState: GameSate = .setup
+    var doesHighestScoreWin: Bool = true
+    
+    /// Определяет список победителей
+    var winners: [Player] {
+        // Если игра не законченна то выходим и список пуст
+        guard gameState == .gameOver else { return [] }
+        
+        // Инициализируем переменную
+        var winningScore = 0
+        if doesHighestScoreWin {
+            // Если побеждает те у кого больше очков от начинаем с минимального значения
+            winningScore = Int.min
+            for player in players { // идем по игрокам и ищем у кого больше очков
+                winningScore = max(winningScore, player.score)
+            }
+        } else {
+            // Если побеждает те у кого меньше очков от начинаем с максимального значения
+            winningScore = Int.max
+            for player in players { // идем по игрокам и ищем у кого меньше очков
+                winningScore = min(winningScore, player.score)
+            }
+        }
+        
+        // возвращаем тех кто соответствует условию
+        // если игроков с одинаково меньшим или большим кол-вом очков несколько то возвращаем всех
+        return players.filter { player in
+            player.score == winningScore
+        }
+        
+    }
+    
+    mutating func resetScores(to newValue: Int) {
+        for index in 0..<players.count {
+            players[index].score = newValue
+        }
+    }
+}
